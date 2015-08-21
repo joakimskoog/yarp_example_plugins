afc0273d43f938175983966c6d745131412dcf8a
joakimskoog <joakimskog@mail.nu>
2015-08-21 13:14:09 +0200

    First commit which contains a ChromePasswordRetriever. This repo will contain example plugins that can be used in the YARP project.

---
+++ YarpExamplePlugins/ChromeYarpPlugin/ChromePasswordRetriever.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Yarp.Plugins.Api;
+
+namespace ChromeYarpPlugin
+{
+    public class ChromePasswordRetriever : IYarpPlugin
+    {
+        private const string PathToPasswordsFile = "Google\\Chrome\\User Data\\Default\\Login Data";
+
+        public void Activate()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Deactivate()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<PasswordRetrievalResult> RetrievePasswords()
+        {
+            var localAppDataFolder = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            var pathToPasswordsFile = Path.Combine(localAppDataFolder, PathToPasswordsFile);
+
+            //Decrypt password file here
+
+
+            return Enumerable.Empty<PasswordRetrievalResult>();
+        }
+    }
+}
